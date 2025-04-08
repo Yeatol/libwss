793ed42ffa4bf40f6d4b46e372ad09616f4a1e37
yeatol <vim@vimer.org>
2025-04-09 01:55:08 +0800

    .

--- wssclient/main.cpp
+++ wssclient/main.cpp
@@ -344,10 +344,10 @@ int main()
     cout << "send " << send_size << endl;
     
 
-    int busypoll = 1000 * 10;
-    setsockopt(fd, SOL_SOCKET, SO_BUSY_POLL, &busypoll, sizeof(int));
+    //int busypoll = 1000 * 10;
+    //setsockopt(fd, SOL_SOCKET, SO_BUSY_POLL, &busypoll, sizeof(int));
 
-    //fcntl(fd, F_SETFL, O_NONBLOCK);
+    fcntl(fd, F_SETFL, O_NONBLOCK);
 
     vector<uint8_t> buff(1024 * 1024 * 4);
 
