27a981e57dece44f71f7d4d47491f98087c96545
yeatol <vim@vimer.org>
2025-04-08 14:00:05 +0800

    .

--- wssclient/main.cpp
+++ wssclient/main.cpp
@@ -30,9 +30,8 @@ int main()
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 
-    SSL_CTX* ctx = SSL_CTX_new(DTLS_client_method());
+    SSL_CTX* ctx = SSL_CTX_new(TLSv1_2_client_method());
     SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, nullptr);
-    SSL_CTX_set_min_proto_version(ctx, TLS1_VERSION);
 
     SSL* ssl = SSL_new(ctx);
     
